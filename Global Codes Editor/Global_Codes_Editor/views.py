9be24b15b350587aebf855224f4da504fcec77b1
Chris Ekin <cjekin@gmail.com>
2015-08-23 14:07:14 +0100

    Added missing templates etc
    Added the missing static files from another Visual Studio project

---
+++ Global Codes Editor/Global_Codes_Editor/views.py
@@ -0,0 +1,45 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from functools import wraps
+from flask import render_template, request, Response
+from ChrisFirstProjecct import app
+import auth
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+@app.route('/secret-page')
+@requires_auth
+def secret_page():
+    return render_template('secret_page.html')
